787570a82f57d8b6ff673d782f0e310024a69ca2
EtienneKuenzel <etienne_kuenzel@web.de>
2024-10-24 00:08:37 +0200

    Reduce Code Size

--- cifar10_tutorial.py
+++ cifar10_tutorial.py
@@ -33,17 +33,20 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         # Convolutional + Max-Pooling Layers
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=1)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1)  # kernel size reduced to 3
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  # pool stride changed to 2
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1)
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  # pool stride changed to 2
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1)
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)  # pool stride changed to 2
+
         # Fully Connected Layers
-        self.fc1 = nn.Linear(in_features=32 * 21 * 21, out_features=16)
+        # Adjusted in_features based on pooling and reduced spatial size
+        self.fc1 = nn.Linear(in_features=32 * 2 * 2, out_features=16)
         self.fc2 = nn.Linear(in_features=16, out_features=16)
         self.fc3 = nn.Linear(in_features=16, out_features=2)
 
+        # Optional Dropout Layer to improve generalization
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
@@ -71,9 +74,7 @@ def filter_data(dataset, labels_to_keep):
     filtered_targets = np.array(filtered_targets)
 
     # Create a new CIFAR10 dataset with the filtered data and targets
-    new_dataset = datasets.CIFAR100(root=dataset.root, train=dataset.train,
-                                   transform=dataset.transform, target_transform=dataset.target_transform,
-                                   download=False)
+    new_dataset = datasets.CIFAR100(root=dataset.root, train=dataset.train,transform=dataset.transform, target_transform=dataset.target_transform,download=False)
     new_dataset.data = filtered_data
     new_dataset.targets = filtered_targets.tolist()
 
@@ -92,18 +93,10 @@ if __name__ == '__main__':
     # Define transformations
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     # Load the CIFAR-100 dataset
-    trainsets = []
-    testsets = []
     trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
     testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
 
-    for x in range(1000): #max 50
-        labels_to_keep = [(0+2*x)%100, (1+2*x)%100]
-        # Apply the filter to the train and test sets and add their dataloader to the array
-        trainsets.append(torch.utils.data.DataLoader(filter_data(trainset, labels_to_keep), batch_size=batch_size, shuffle=True, num_workers=2))
-        testsets.append(torch.utils.data.DataLoader(filter_data(testset, labels_to_keep), batch_size=batch_size, shuffle=True, num_workers=2))
-        # get some random training images
-        #imshow(torchvision.utils.make_grid(next(iter(trainsets[-1]))[0]))
+
     # Instantiate the network
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     net = Net()
@@ -113,8 +106,14 @@ if __name__ == '__main__':
 
     # Train the network
     a= 0
-    for trainloader, testloader in zip(trainsets, testsets):
-        a+=1
+    for x in range(1000): #max 50
+        labels_to_keep = [(0+2*x)%100, (1+2*x)%100]
+        a += 1
+        # Apply the filter to the train and test sets and add their dataloader to the array
+        trainloader = torch.utils.data.DataLoader(filter_data(trainset, labels_to_keep), batch_size=batch_size, shuffle=True, num_workers=2)
+        testloader  = torch.utils.data.DataLoader(filter_data(testset, labels_to_keep), batch_size=batch_size, shuffle=True, num_workers=2)
+        # get some random training images
+        #imshow(torchvision.utils.make_grid(next(iter(trainsets[-1]))[0]))
         for epoch in range(5):  # 20 good time to get used to data pair
             for i, data in enumerate(trainloader, 0):
                 inputs, labels = data
@@ -132,7 +131,6 @@ if __name__ == '__main__':
 
         with open('accuracy_results.csv', mode='a', newline='') as file:
             writer = csv.writer(file)
-
             # Check accuracy on the whole dataset
             correct = 0
             total = 0
