f036dfc3b02206fc0ef70a9a05b6bf447b73d9bd
EtienneKuenzel <etienne_kuenzel@web.de>
2024-10-23 21:17:16 +0200

    Add Results Folder + Add CSV Output

--- cifar10_tutorial.py
+++ cifar10_tutorial.py
@@ -7,9 +7,9 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 import numpy as np
 from torchvision import datasets
-
+import csv
 # Define a Convolutional Neural Network
-class Net(nn.Module):
+class Nettest(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
@@ -28,6 +28,34 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # Convolutional + Max-Pooling Layers
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=1)
+        # Fully Connected Layers
+        self.fc1 = nn.Linear(in_features=32 * 21 * 21, out_features=16)
+        self.fc2 = nn.Linear(in_features=16, out_features=16)
+        self.fc3 = nn.Linear(in_features=16, out_features=2)
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
+
+        # Flatten the output for the fully connected layers
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 def filter_data(dataset, labels_to_keep):
     filtered_data = []
@@ -60,26 +88,22 @@ def imshow(img):
 
 if __name__ == '__main__':
     # Define the batch size
-    batch_size = 4
-
+    batch_size = 10
     # Define transformations
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-    # Load the CIFAR-10 dataset
+    # Load the CIFAR-100 dataset
+    trainsets = []
+    testsets = []
     trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform)
     testset = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform)
 
-    # Filter the dataset to only include labels 0 (plane) and 1 (car)
-
-    trainsets = []
-    testsets = []
-    classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    # Labels to keep (0: plane, 1: car)
-    for x in range(40): #max 50
-        labels_to_keep = [0+2*x, 1+2*x]
+    for x in range(1000): #max 50
+        labels_to_keep = [(0+2*x)%100, (1+2*x)%100]
         # Apply the filter to the train and test sets and add their dataloader to the array
         trainsets.append(torch.utils.data.DataLoader(filter_data(trainset, labels_to_keep), batch_size=batch_size, shuffle=True, num_workers=2))
         testsets.append(torch.utils.data.DataLoader(filter_data(testset, labels_to_keep), batch_size=batch_size, shuffle=True, num_workers=2))
+        # get some random training images
+        #imshow(torchvision.utils.make_grid(next(iter(trainsets[-1]))[0]))
     # Instantiate the network
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     net = Net()
@@ -88,12 +112,13 @@ if __name__ == '__main__':
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
     # Train the network
+    a= 0
     for trainloader, testloader in zip(trainsets, testsets):
-        running_loss = 0.0
-        for epoch in range(5):  # loop over the dataset multiple times
+        a+=1
+        for epoch in range(5):  # 20 good time to get used to data pair
             for i, data in enumerate(trainloader, 0):
                 inputs, labels = data
-                labels = (labels == labels.max()).long()  # This will be 1 where the condition is true, 0 otherwise
+                labels = (labels == labels.max()).long()  # Convert labels to binary as required
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -103,32 +128,24 @@ if __name__ == '__main__':
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
-
-                # print statistics
-                running_loss += loss.item()
-                if i % 2000 == 1999:    # print every 2000 mini-batches
-                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
-                running_loss = 0.0
-
         print('Finished Training' + str())
 
-        # Save the trained model
-        #PATH = './cifar_net.pth'
-        #torch.save(net.state_dict(), PATH)
-        # Reload the model for inference
-        #net = Net()
-        #net.load_state_dict(torch.load(PATH))
+        with open('accuracy_results.csv', mode='a', newline='') as file:
+            writer = csv.writer(file)
 
-        # Check accuracy on the whole dataset
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for data in testloader:
-                images, labels = data
-                labels = (labels == labels.max()).long()
-                _, predicted = torch.max(net(images), 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+            # Check accuracy on the whole dataset
+            correct = 0
+            total = 0
+            with torch.no_grad():
+                for data in testloader:
+                    images, labels = data
+                    labels = (labels == labels.max()).long()
+                    _, predicted = torch.max(net(images), 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
 
-        print(f'Accuracy of the network in differentiating the two images: {100 * correct // total} %')
+            accuracy = 100 * correct / total
+            print(f'Accuracy of the network in differentiating the two images: {accuracy:.2f} %')
 
+            # Write the accuracy to the CSV file
+            writer.writerow([a, accuracy])
